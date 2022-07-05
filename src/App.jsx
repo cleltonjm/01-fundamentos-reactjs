fd1e56a50a48f0daf8d64900a444fa3df83d5c34
Clelton Mielke <60109916+cleltonjm@users.noreply.github.com>
2022-07-04 21:16:10 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import { Header } from './components/Header';
+import { SideBar } from './components/SideBar';
 import { Post } from './Post';
 
+import styles from './App.module.css';
 import './global.css';
 
 export function App() {
   return (
     <>
       <Header />
-      <Post 
-        author="Clelton José Mielke" 
-        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores doloribus fugiat at laboriosam odio, dolorum provident nihil expedita doloremque dolor distinctio cum recusandae! Quos reprehenderit voluptatibus impedit exercitationem sunt vero?" 
-      />
-      <Post 
-        author="Diego Fernandes"
-        content="Um novo post muito legal!"
-      />
+      
+      <div className={styles.wrapper}>
+        <SideBar />
+        <main>
+          <Post 
+            author="Clelton José Mielke" 
+            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores doloribus fugiat at laboriosam odio, dolorum provident nihil expedita doloremque dolor distinctio cum recusandae! Quos reprehenderit voluptatibus impedit exercitationem sunt vero?" 
+          />
+          <Post 
+            author="Diego Fernandes"
+            content="Um novo post muito legal!"
+          />
+        </main>
+      </div>
     </>
   )
 }
