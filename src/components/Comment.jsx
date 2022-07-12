3ceaf4b15a6e6de662201de55b7b5cc753f18358
Clelton Mielke <60109916+cleltonjm@users.noreply.github.com>
2022-07-11 21:01:24 -0300

    Add files via upload

---
+++ src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/cleltonjm.png" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Clelton José Mielke</strong>
+                            <time title="11 de Maio às 08:00h" dateTime="2022-05-11 08:00:13">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title="Deletar comentário">
+                            <Trash size={20} />
+                        </button>
+                    </header>
+                    
+                    <p>Muito bom Devon, parabéns!</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    );
+}
